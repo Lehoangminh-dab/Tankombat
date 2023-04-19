936c29c50efef2e643f77501742193958cebc340
Le Hoang Minh <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-04-19 15:03:48 +0700

    Added Gameplay Sound Effects

--- SoundManager.cpp
+++ SoundManager.cpp
@@ -1,1 +1,9 @@
-#include "SDL_mixer.h"
+#include "SoundManager.hpp"
+
+// TO_DO: Add Music
+
+// The sound effects that will be used
+Mix_Chunk* tankIdleSound = NULL;
+Mix_Chunk* tankMovingSound = NULL;
+Mix_Chunk* tankShootingSound = NULL;
+Mix_Chunk* projectileExplosionSound = NULL;
---
+++ SoundManager.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "SDL_mixer.h"
+
+class SoundManager
+{
+
+};
